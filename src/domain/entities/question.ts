4674c5e4a651f02cc34af6292f258bd2738fe97c
IgoorToorres <igorevangelista121@gmail.com>
2026-04-06 14:44:13 -0300

    feat: criação de obejto com aproveitamento de constructor global

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,31 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Entity } from '../../core/entities/entity'
 import { Slug } from './value-objects/slug'
+import { Optional } from '@/core/types/optional'
 
 interface questionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
   slug: Slug
-  authorId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Question extends Entity<questionProps> {}
+export class Question extends Entity<questionProps> {
+  static create(
+    props: Optional<questionProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    return question
+  }
+}
