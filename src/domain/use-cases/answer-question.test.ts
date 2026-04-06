22d9899fb4bbe6d99d1316a566d6071cc9445dc9
IgoorToorres <igorevangelista121@gmail.com>
2026-04-06 13:45:31 -0300

    initial commit

---
+++ src/domain/use-cases/answer-question.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repository/answer-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswerRepository: AnswerRepository ={
+    create: async (answer: Answer) => {
+        return;
+    }
+}
+
+test("create an answer", async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+
+    const answer = await answerQuestion.execute({ instructorId: "1", questionId: "1", content: "Nova resposta" })
+
+    expect(answer.content).toEqual('Nova resposta')
+})
